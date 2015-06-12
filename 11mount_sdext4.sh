5cba9189b0c66460f1f5c386027ba68b7f569548
EternalPhane <zzy_2626@126.com>
2015-06-12 20:22:08 +0800

    solved following bug(s):   1. LOG_DIR did not exist.

--- 11mount_sdext4.sh
+++ 11mount_sdext4.sh
@@ -2,23 +2,28 @@
 # added by eternalphane
 # edit with Visual Studio Code 0.3.0
 
-LOG=/data/log/mountlog.log
-ERR_LOG=/data/log/mounterr.log
+LOG_DIR=/data/log
+MOUNT_DIR=/data/sdext4
+LOG=${LOG_DIR}/mountlog.log
+ERR_LOG=${LOG_DIR}/mounterr.log
 echo "$(date) mounting..." > $LOG
 sleep 1
 
-if [ -f $ERR_LOG ];then
-  rm $ERR_LOG
+if [ ! -d ${LOG_DIR} ];then
+  mkdir ${LOG_DIR}
 fi
-if [ ! -d /data/sdext4 ];then
-  mkdir /data/sdext4
+if [ -f ${ERR_LOG} ];then
+  rm ${ERR_LOG}
+fi
+if [ ! -d ${MOUNT_DIR} ];then
+  mkdir ${MOUNT_DIR}
 fi
 sleep 1
 
-mount -t ext4 -o rw /dev/block/vold/179:34 /data/sdext4 1>>$LOG 2>$ERR_LOG
+mount -t ext4 -o rw /dev/block/vold/179:34 ${MOUNT_DIR} 1>>$LOG 2>$ERR_LOG
 echo
 
-mount -t ext4 -o rw /dev/block/mmcblk1p2 /data/sdext4 1>>$LOG 2>>$ERR_LOG
+mount -t ext4 -o rw /dev/block/mmcblk1p2 ${MOUNT_DIR} 1>>$LOG 2>>$ERR_LOG
 echo
 
 mount >> $LOG
